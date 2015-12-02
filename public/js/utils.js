564ebc04340a7a1891a4fc950e3458a14e42a2df
David Ung <d3ung@ucsd.edu>
2015-12-02 00:26:30 -0800

    Remove Error Tracking and Starting Analytics

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,14 @@
 function checkLogin() {
     Parse.initialize("V6NcQkeFHBu6SOcSYJptWFgKzgOiuc2ywEXnmL31", "Xw3yYjXIFL6tVLwN3vhmPJMYLmd4AiJI3mRUjl1l");
+	var dimensions = {
+  // What type of news is this? 
+  category: 'politics',
+  // Is it a weekday or a weekend?
+  dayType: 'weekday'
+};
+// Send the dimensions to Parse along with the 'search' event
+ 
+Parse.Analytics.track('read', dimensions);
     if(window.location.pathname.split("/").pop() != 'login.html') {
         if (Parse.User.current() == null) {
             window.location.href = "./login.html";
